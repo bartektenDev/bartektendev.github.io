38969d2d9b1f5538ef3295b45ff452051fbaa543
bartektenDev <bartek8991@live.com>
2019-07-27 14:30:28 -0500

    fixed redirect bug2

--- safemap/assets/js/mapjsdata.js
+++ safemap/assets/js/mapjsdata.js
@@ -8,7 +8,7 @@ var damagedCityRadius = {
 };
 
 function initMap() {
-    window.location.href = 'http://bartektendev.github.io/safemap/usermap.html#popuploading';
+    //window.location.href = 'http://bartektendev.github.io/safemap/usermap.html#popuploading';
     map = new google.maps.Map(document.getElementById('map'), {
       zoom: 2,
       center: {lat: 50, lng: 50},
@@ -342,7 +342,7 @@ function initMap() {
     map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(
         mapTypeControlDiv);
 
-    setTimeout(function(){ document.getElementById("finishedLoadingClosebtn").click(); }, 2000);
+    //setTimeout(function(){ document.getElementById("finishedLoadingClosebtn").click(); }, 2000);
 }
 
 function initZoomControl(map) {
